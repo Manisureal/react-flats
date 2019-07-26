4f2a72bb681aa1d437b0ee98afcec67ddd5b030d
manisureal <mansoor.zaman@sage.com>
2019-07-26 15:26:04 +0100

    created component lifecycle methods for FlatList Component

--- src/components/flat-list/flat-list.jsx
+++ src/components/flat-list/flat-list.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import Flats from '../../../data/flats';
 import './flat-list.scss'
 
 class FlatList extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			flats: Flats
+		}
+	}
+	
+	displayFlats(){
+		//Implement this after Flat Component is created
+		//this.state.flats.map(flat => )
+	}
+
+	componentWillMount(){
+		console.log('COMPONENT WILL MOUNT')
+	}
+
+	componentDidMount(){
+		console.log('COMPONENT DID MOUNT')
+	}
+
 	render(){
+		console.log('COMPONENT WILL RENDER')
+		console.log(this.state.flats)
 		return null;
 	}
 }
