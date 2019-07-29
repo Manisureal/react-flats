2e079f4a8903f8d6b0a80834818bd37b48d16d93
manisureal <mansoor.zaman@sage.com>
2019-07-29 11:58:06 +0100

    created flat component

--- src/components/flat/flat.jsx
+++ src/components/flat/flat.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './flat.scss';
 
-class Flat extends Component {
+class Flat extends React.Component {
 	render(){
-		return null;
+
+	  const imgUrl = {
+        backgroundImage: `url(${this.props.imageUrl})`,
+        backgroundPosition: 'center',
+  		backgroundSize: 'cover',
+  		backgroundRepeat: 'no-repeat'
+      };
+
+	  return(
+	  	<div className="flat" style={imgUrl}>
+	  	  <p>{ this.props.priceCurrency + this.props.price }</p>
+	  	  <p>{ this.props.name }</p>
+	  	  { <img /> }
+	  	</div>
+	  );
 	}
 }
 
