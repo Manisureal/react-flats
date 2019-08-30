ca9ea383d95b49851247cfa1a84fc64287bcc522
manisureal <mansoor.zaman@sage.com>
2019-08-30 11:05:28 +0100

    updated dead image urls and added hover state to flats

--- data/flats.js
+++ data/flats.js
@@ -1,7 +1,8 @@
 const flats = [
   {
     name: "Charm at the Steps of the Sacre Coeur/Montmartre",
-    imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+    // imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+    imageUrl: "https://images.unsplash.com/photo-1498450067505-1e6cec4224fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
     price: 164,
     priceCurrency: "EUR",
     lat: 48.884211,
@@ -9,7 +10,7 @@ const flats = [
   },
   {
     name: "Trendy Apt in Buttes Montmartre",
-    imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+    imageUrl: "https://images.unsplash.com/photo-1479705879471-5afa19ebdcc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
     price: 200,
     priceCurrency: "EUR",
     lat: 48.885707,
@@ -17,7 +18,7 @@ const flats = [
   },
   {
     name: "Super 60m2 in trendy neighborhood!",
-    imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg",
+    imageUrl: "https://images.unsplash.com/photo-1497238932193-904bf1749495?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
     price: 150,
     priceCurrency: "EUR",
     lat: 48.885312,
@@ -25,7 +26,7 @@ const flats = [
   },
   {
     name: "Splendide terrasse vue imprenable",
-    imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat4.jpg",
+    imageUrl: "https://images.unsplash.com/photo-1469022563428-aa04fef9f5a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2253&q=80",
     price: 115,
     priceCurrency: "EUR",
     lat: 48.88184,
@@ -33,7 +34,7 @@ const flats = [
   },
   {
     name: "Superbe vue à 2 min du Sacré Coeur",
-    imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat5.jpg",
+    imageUrl: "https://images.unsplash.com/photo-1565329921943-7e537b7a2ea9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80",
     price: 135,
     priceCurrency: "EUR",
     lat: 48.888839,
@@ -41,7 +42,7 @@ const flats = [
   },
   {
     name: "Bohemian and Chic in Paris",
-    imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat6.jpg",
+    imageUrl: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
     price: 90,
     priceCurrency: "EUR",
     lat: 48.827855,
