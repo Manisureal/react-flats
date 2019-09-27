d3a6f5d64a9e8059e9176371dbbb50166ca898f5
manisureal <mansoor.zaman@sage.com>
2019-09-27 12:34:13 +0100

    updated the app file to be able to display markers through the use of state

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
 import FlatList from '../flat-list/flat-list';
+import GoogleMapReact from 'google-map-react';
 
 import './app.scss'
 
 class App extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			allFlats: null
+		}
+	}
+
+	retrieveFlats = (f) => {
+		this.setState({ allFlats: f });
+	}
+
+	returnFlats(){
+		// stateless functional component - does not have a state and takes in either an object or a prop
+		const flatMarkers = []
+		const AnyReactComponent = ( {text} ) => (
+			<div className="marker">{text}</div>
+
+		);
+
+		if (this.state.allFlats !== null){
+			this.state.allFlats.forEach(({ lat, lng, name }) =>{
+				flatMarkers.push(<AnyReactComponent lat={lat} lng={lng} text={name} />)
+			})
+		}
+
+		return flatMarkers
+	}
+
 	render(){
 		return(
 			<>
 				<div className="left-scene">
-					<FlatList />
+					<FlatList getFlatsCallback={this.retrieveFlats} />
 				</div>
 				<div className="right-scene">
+					<GoogleMapReact
+						defaultCenter={{lat: 48.884211, lng: 2.34689}}
+						defaultZoom={11}
+					>
+						{ this.returnFlats() }
+					</GoogleMapReact>
 				</div>
 			</>
 		)
